a976c25590ae588bd38ffbd02c5940af884d30cb
Ursula Jardon <155091638+gar9312@users.noreply.github.com>
2024-05-28 05:00:37 +0000

    CardGen

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+// eslint-disable
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +6,55 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardSuits = ["♦", "♥", "♠", "♣"];
+  const cardNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  updateCard();
+
+  // Agregar un evento de clic al botón de cambio
+  const changeButton = document.getElementById("changeBtn");
+  changeButton.addEventListener("click", function() {
+    updateCard();
+  });
+
+  function updateCard() {
+    let randomSuit = Math.floor(Math.random() * 4);
+    let randomNumber = Math.floor(Math.random() * 12);
+
+    // Obtener los elementos HTML
+    const topCardSide = document.getElementById("topCardSide");
+    const middleCardSide = document.getElementById("middleCardSide");
+    const bottomCardSide = document.getElementById("bottomCardSide");
+
+    // Asignar el contenido HTML a los elementos
+    topCardSide.innerHTML = cardSuits[randomSuit];
+    middleCardSide.innerHTML = cardNumbers[randomNumber];
+    bottomCardSide.innerHTML = cardSuits[randomSuit];
+
+    // Rotar el elemento bottomCardSide
+    bottomCardSide.style.transform = "rotate(180deg)";
+
+    // Cambiar el color del texto según el palo de la carta
+    if (cardSuits[randomSuit] == "♦" || cardSuits[randomSuit] == "♥") {
+      topCardSide.style.color = "red";
+      bottomCardSide.style.color = "red";
+    } else {
+      topCardSide.style.color = "black";
+      bottomCardSide.style.color = "black";
+    }
+  }
 };
